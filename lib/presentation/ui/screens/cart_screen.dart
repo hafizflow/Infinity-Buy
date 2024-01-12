5d5d942ec54d49f3d1f942a3313fc7ff7b2a3b7b
HafizSp <hafizsp45@gmail.com>
2024-01-13 04:59:14 +0600

    cart screen ui complete

--- lib/presentation/ui/screens/cart_screen.dart
+++ lib/presentation/ui/screens/cart_screen.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:infinity_buy/presentation/state_holders/main_bottom_nav_controller.dart';
+import 'package:infinity_buy/presentation/ui/utility/app_colors.dart';
+
+import '../widgets/cart_product_item.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,6 +15,84 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (_) {
+        Get.find<MainBottomNavController>().backToHome();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Get.find<MainBottomNavController>().backToHome();
+            },
+          ),
+          title: const Text("Carts"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return const CartProductItem();
+                  },
+                ),
+              ),
+            ),
+            totalPriceAndCheckOutSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container get totalPriceAndCheckOutSection {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor.withOpacity(0.15),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Total Price",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "\$1000,000.00",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("Checkout"),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/presentation/ui/widgets/cart_product_item.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+import '../utility/app_colors.dart';
+import '../utility/assets_path.dart';
+
+class CartProductItem extends StatelessWidget {
+  const CartProductItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ValueNotifier<int> numberOfItem = ValueNotifier(1);
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(16),
+        bottomLeft: Radius.circular(16),
+      ),
+      child: Card(
+        elevation: 1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              AssetsPath.dummyShoePng,
+              width: 120,
+              height: 115,
+              fit: BoxFit.cover,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "New Year Special Shoe",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.grey.shade700,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                                const Row(
+                                  children: [
+                                    Text(
+                                      "Color: Black",
+                                      style: TextStyle(
+                                          fontSize: 12, color: Colors.black54),
+                                    ),
+                                    SizedBox(width: 12),
+                                    Text(
+                                      "Size: X",
+                                      style: TextStyle(
+                                          fontSize: 12, color: Colors.black54),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.delete_forever_outlined,
+                              color: Colors.grey.shade600,
+                              size: 26,
+                            ),
+                            padding: const EdgeInsets.all(2),
+                            constraints: const BoxConstraints(),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "\$100",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          ValueListenableBuilder(
+                            valueListenable: numberOfItem,
+                            builder: (context, value, _) {
+                              return itemCount(numberOfItem, value);
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding itemCount(ValueNotifier<int> numberOfItem, int value) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 15.0),
+      child: Wrap(
+        direction: Axis.horizontal,
+        spacing: 4,
+        children: [
+          InkWell(
+            onTap: () {
+              if (numberOfItem.value > 1) {
+                numberOfItem.value--;
+              }
+            },
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(3),
+                ),
+                color: numberOfItem.value == 1
+                    ? AppColors.primaryColor.withOpacity(0.6)
+                    : AppColors.primaryColor,
+              ),
+              child: const Icon(
+                Icons.remove,
+                size: 14,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Text(
+            "$value",
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              if (numberOfItem.value < 10) {
+                numberOfItem.value++;
+              }
+            },
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(3),
+                ),
+                color: numberOfItem.value == 10
+                    ? AppColors.primaryColor.withOpacity(0.6)
+                    : AppColors.primaryColor,
+              ),
+              child: const Icon(
+                Icons.add,
+                size: 14,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
